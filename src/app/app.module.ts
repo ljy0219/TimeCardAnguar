3471771c064c1160fd044dd1837f7ed4157280ad
ljy0219 <answer0219@qq.com>
2021-05-09 21:37:11 +0800

    add userinfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      UserinfoComponent
+   ],
   imports: [
     BrowserModule
   ],
